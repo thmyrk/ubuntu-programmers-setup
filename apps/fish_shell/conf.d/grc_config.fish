613fb6fd953c7272a82e55fdfbac4c2f9b23f868
Sławek Piotrowski <sentinel@atteo.com>
2017-05-30 03:43:54 +0200

    Update list of colored commands

--- apps/fish_shell/conf.d/grc_config.fish
+++ apps/fish_shell/conf.d/grc_config.fish
@@ -1,17 +1,16 @@
 
 # Use GRC only for following commands
 
-set -U grc_plugin_execs cat cvs df diff dig gcc g++ ifconfig \
-       make mount mtr netstat ps tail traceroute \
-       wdiff blkid du dnf docker docker-machine env id ip iostat \
-       last lsattr lsblk lspci lsmod lsof getfacl getsebool ulimit uptime nmap \
-       fdisk findmnt free semanage sar ss sysctl systemctl stat showmount tune2fs \
-       tcpdump tune2fs \
-       vmstat w who
+set -U grc_plugin_execs ant blkid cvs df diff dig docker docker docker-machine du env id ip iostat \
+	gcc g++ ifconfig make mount mtr netstat ps tail traceroute \
+       wdiff        last lsattr lsblk lspci lsmod lsof getfacl getsebool ulimit uptime nmap \
+       fdisk findmnt free semanage sar ss sysctl systemctl stat showmount tune2fs tcpdump  \
+       vmstat w who iptables ldap mvn php pv
 
 for executable in $grc_plugin_execs
     if type -q $executable
         alias $executable "grc $executable"
     end
 end
+
 
