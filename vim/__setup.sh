842463530b3a22798ea32c1353308358e77b5428
SÅawek Piotrowski <sentinel@atteo.com>
2017-06-10 22:58:35 +0200

    Enable support for Xorg clipboard from ViM
    See: http://vim.wikia.com/wiki/Accessing_the_system_clipboard

--- vim/__setup.sh
+++ vim/__setup.sh
@@ -2,27 +2,33 @@
 
 trap "exit 1" ERR
 
-set -x
-
-sudo apt-get -y install curl git vim
+echo "Installing ViM executable"
+# Install vim-gtk which should have support for Xorg clipboard
+sudo apt-get -y install curl git vim-gtk3
 sudo apt-get -y install aspell-pl
 
-# Install Vundle for plugin management
 mkdir -p ~/.vim/{bundle,tmp,spell,undodir}
+
+echo "Installing Vundle for plugin management"
 if ! [[ -d ~/.vim/bundle/Vundle.vim ]]; then
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 
-# Install spell checking files
+echo "Installing ViM spell checking files"
 for lang in pl.utf-8 en.utf-8; do
     curl http://ftp.vim.org/pub/vim/runtime/spell/$lang.spl > ~/.vim/spell/$lang.spl
 done
 
-# Install custom .vimrc
+echo "Installing custom .vimrc"
 cp -r .vimrc .vim ~/
 
 vim +PluginUpdate +'q!' +'q!'
 
+
+if vim --version | grep -qF -- "-clipboard"; then
+	echo "Warning: ViM compiled without Xorg clipboard support"
+	echo "See this link for more info: http://vim.wikia.com/wiki/Accessing_the_system_clipboard"
+fi
 
 
 
